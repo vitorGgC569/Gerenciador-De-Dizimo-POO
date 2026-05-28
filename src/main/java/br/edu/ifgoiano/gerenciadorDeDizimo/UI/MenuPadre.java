5133aeaf9eb8c8a5e8d149f8c6b7f531452695ae
Vitor Gabriel Gomes <vitorggc9@gmail.com>
2026-05-28 00:42:08 -0300

    Fix: PackageName

---
+++ src/main/java/br/edu/ifgoiano/gerenciadorDeDizimo/UI/MenuPadre.java
@@ -0,0 +1,32 @@
+package br.edu.ifgoiano.gerenciadorDeDizimo.UI;
+
+import javax.swing.*;
+
+public class MenuPadre {
+
+    public void UIMenuPadre() {
+        loopUI:
+        while (true) {
+            String op = JOptionPane.showInputDialog(null, """
+                    ===================================================
+                    MENU PADRE
+                    ===================================================
+                    [1]: Ver todas as doações
+                    [2]: Voltar
+                    ===================================================
+                    Escolha uma Opção:
+                    """);
+
+            switch (op) {
+                case "1":
+                    //listar todas as doações do banco de dados
+                    break;
+                case "2":
+                    JOptionPane.showMessageDialog(null, "Voltando...");
+                    break loopUI;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção Inválida!");
+            }
+        }
+    }
+}
