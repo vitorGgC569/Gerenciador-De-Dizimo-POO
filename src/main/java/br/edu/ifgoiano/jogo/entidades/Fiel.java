2806c20cd81d19b003dadf121721084c4db788ef
Vitor Gabriel Gomes <vitorggc9@gmail.com>
2026-05-27 22:15:05 -0300

    Adiciona Padre e Fiel

---
+++ src/main/java/br/edu/ifgoiano/jogo/entidades/Fiel.java
@@ -0,0 +1,20 @@
+public class Fiel {
+    private Date dataBatismo;
+    private boolean isAdmin;
+
+    public Date getDataBatismo() {
+        return dataBatismo;
+    }
+
+    public void setDataBatismo(Date dataBatismo) {
+        this.dataBatismo = dataBatismo;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
---
+++ src/main/java/br/edu/ifgoiano/jogo/entidades/Padre.java
@@ -0,0 +1,20 @@
+public class Padre {
+    private Double salario;
+    private Date data_Ordenacao;
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public Date getData_Ordenacao() {
+        return data_Ordenacao;
+    }
+
+    public void setData_Ordenacao(Date data_Ordenacao) {
+        this.data_Ordenacao = data_Ordenacao;
+    }
+}
