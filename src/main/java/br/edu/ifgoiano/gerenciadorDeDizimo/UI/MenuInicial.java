b6a2a0b0048d807d3818db314bf7354b57d56c8c
hadawardbs <edubs008@gmail.com>
2026-05-28 01:48:19 -0300

    Atualização Interface

--- src/main/java/br/edu/ifgoiano/gerenciadorDeDizimo/UI/MenuInicial.java
+++ src/main/java/br/edu/ifgoiano/gerenciadorDeDizimo/UI/MenuInicial.java
@@ -14,41 +14,43 @@ public class MenuInicial {
      * Inicia a Interface do Menu Inicial
      */
     public void UiMenuInicial(){
-        String op = JOptionPane.showInputDialog("""
-                ===================================================
-                                    MENU INICIAL
-                ===================================================
-                [1]: Fazer Login
-                [2]: Cadastrar Usuário
-                [3]: Buscar Usuário
-                [4]: Atualizar Usuário
-                [5]: Deletar Usuário
-                [6]: Sair
-                ===================================================
-                Escolha uma Opção:
-                """);
-        switch (op) {
-            case "1":
-                menuLogin.UIMenuLogin();
-                break;
-            case "2":
-                menuCadastrarUsuario.UIMenuCadastrarUsuario();
-                break;
-            case "3":
-                menuBuscarUsuario.UIMenuBuscarUsuario();
-                break;
-            case "4":
-                menuAtualizarUsuario.UIMenuAtualizarUsuario();
-                break;
-            case "5":
-                menuDeletarUsuario.UIMenuDeletarUsuario();
-                break;
-            case "6":
-                JOptionPane.showMessageDialog(null, "Saindo...");
-                System.exit(0);
-                break;
-            default:
-                JOptionPane.showMessageDialog(null, "Opção Inválida!");
+        while (true) {
+            String op = JOptionPane.showInputDialog("""
+                    ===================================================
+                                        MENU INICIAL
+                    ===================================================
+                    [1]: Fazer Login
+                    [2]: Cadastrar Usuário
+                    [3]: Buscar Usuário
+                    [4]: Atualizar Usuário
+                    [5]: Deletar Usuário
+                    [6]: Sair
+                    ===================================================
+                    Escolha uma Opção:
+                    """);
+            switch (op) {
+                case "1":
+                    menuLogin.UIMenuLogin();
+                    break;
+                case "2":
+                    menuCadastrarUsuario.UIMenuCadastrarUsuario();
+                    break;
+                case "3":
+                    menuBuscarUsuario.UIMenuBuscarUsuario();
+                    break;
+                case "4":
+                    menuAtualizarUsuario.UIMenuAtualizarUsuario();
+                    break;
+                case "5":
+                    menuDeletarUsuario.UIMenuDeletarUsuario();
+                    break;
+                case "6":
+                    JOptionPane.showMessageDialog(null, "Saindo...");
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção Inválida!");
+            }
         }
     }
 }
